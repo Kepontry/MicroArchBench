7aab9a4656515d0f2b6eb6f0ae0b6a80c4deed9f
Kepontry <zjpzhoujiapeng@163.com>
2024-12-22 18:27:53 +0800

    res: first try

---
+++ res/report.mk
@@ -0,0 +1,70 @@
+Frequency: 4.77497 GHz, Cycle time: 0.209426 ns
+
+ALU width: 5.36897
+
+Nop width: 7.67966
+
+BRU width: 2.87339
+
+FPU width: 3.98967
+
+Rename width: 6.15963
+
+Load width: 2.93346
+
+Store width: 1.97573
+
+AGU width: 3.93056
+
+Mul latency: 3.07016
+
+Mul bandwidth: 2.60504
+
+Div(unsigned) latency: 16.223 (plz compute)
+
+Div(signed) latency: 15.9323 (plz compute)
+
+Div(unsigned) bandwidth: 0.0954374
+
+Fadd latency: 2.00573
+
+Fadd bandwidth: 1.98255
+
+Fma latency: 3.99523
+
+Fma bandwidth: 1.93252
+
+Fmul latency: 2.99929
+
+Fmul bandwidth: 1.79419
+
+Fdiv latency: 11.9893
+
+Fdiv bandwidth: 0.497014
+
+Fsqrt latency: 18.7538
+
+Mov imm zero: 3.95531
+
+Mov imm one: 3.93141
+
+Mov chain: 7.02902
+
+Mov single: 6.88476
+
+Fmov single: 7.97583
+
+Mov self: 0.998411
+
+Mov self mul: 5.38685
+
+Mov bounce: 7.58839
+
+Sub eli: 7.44843
+
+Xor_eli: 7.68189
+
+Pointer chasing: 4.00165
+
+No pointer chasing: 4.99884
+
